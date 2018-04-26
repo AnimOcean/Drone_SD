21e94271bddaab8bd7405b744c96d42de4888277
elmcdaniel <38763287+elmcdaniel@users.noreply.github.com>
2018-04-26 14:03:48 -0400

    initial commit
    -image_corr.m- correlation between images
    -stitch.m- original rgb stitch

---
+++ Functions/image_corr.m
@@ -0,0 +1,93 @@
+%% Correlation between images 
+
+clear all 
+clc 
+close all
+
+%%
+imageDir = fullfile('C:\Users\liz_surface5\Desktop\Senior Design\practice photos\main_building5');
+images = imageDatastore(imageDir);
+new_folder= fullfile('C:\Users\liz_surface5\Desktop\Senior Design\practice photos\main_building5\reorder');
+% I1 = readimage(images,10);
+% I1a = rgb2gray(I1);
+% figure
+% % imshow(I1)
+% imshow(I1a)
+% I2 = readimage(images,11);
+% I2a = rgb2gray(I2);
+% corr=corr2(I1a,I2a)
+% corr1=corr2(I1a,I1a)
+% figure 
+% imshow(I2a)
+
+% n=length(images.Files);
+n=5;
+R=zeros(1,n);
+
+
+for i=1:n
+    Img = readimage(images, i);
+    Img = rgb2gray(Img);
+    I{i} = Img;
+end
+
+testCorr = zeros(length(I));
+for i = 1:length(I)
+    for j = 1:length(I)
+        if i ~= j
+            testCorr(i,j) = corr2(I{i},I{j});
+        end
+    end    
+end
+% ph=zeros(n);
+% for i = 1:length(I)
+%     for j=
+%    [M,I]=max(testCorr)
+%    ph(I,i)=1;
+% %    for 
+% %    for j=
+% %    end
+% end
+
+fprintf('done')
+
+
+% for j=2:n
+%     R(1,j)=corr2(I{j-1},I{j})
+% %     [M,I]=max(R)
+% %     baseFileName=images.Files(I)
+% %     newBaseFileName= sprintf('%s_image.PNG', j);
+% %     existingFullFileName = fullfile(imageDir, baseFileName);
+% %     newFullFileName = fullfile(new_folder, newBaseFileName);
+% %     movefile(existingFullFileName{1}, newFullFileName);
+% end
+
+% [folder, baseFileName, ext] = fileparts(oldFileName);
+% % Change base file name and extension.
+% newBaseFileName= sprintf('%s_images.PNG', baseFileName);
+% newFullFileName = fullfile(folder, newBaseFileName);
+
+
+% numberOfImageFiles = length(baseFileNames);
+% 	if numberOfImageFiles >= 1
+% 		% Go through all those files.
+% 		for f = 1 : numberOfImageFiles
+% 			existingFullFileName = fullfile(thisFolder, baseFileNames(f).name);
+% 			if isdir(existingFullFileName)
+% 				% Skip folders . and ..
+% 				continue;
+% 			end
+% 			% Get the last character of the folder.  It should be a number from 1 to 4.
+% 			lastDigit = thisFolder(end);
+% 			% Create a new name for it.
+% 			newBaseFileName = sprintf('%s_images_%d.png', lastDigit, f-1);
+% 			newFullFileName = fullfile(thisFolder, newBaseFileName);
+% 			fprintf('     Renaming file %s to %s\n', existingFullFileName, newFullFileName);
+% 			% Do the actual renaming.
+% 			movefile(existingFullFileName, newFullFileName);
+% 		end
+% 	else
+% 		fprintf('     Folder %s has no files in it.\n', thisFolder);
+% 	end
+
+%Look into using Simulink as the middle point of the project
