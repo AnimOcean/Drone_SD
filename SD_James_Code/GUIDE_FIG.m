db34b7e926111f6ab58474005a9b7408f6b0a85e
James Blackmer <jamescb@msn.com>
2018-04-30 19:17:48 -0400

    Updated GUI
    Added buttons to gui for several functionalities

--- SD_James_Code/GUIDE_FIG.m
+++ SD_James_Code/GUIDE_FIG.m
@@ -22,7 +22,7 @@ function varargout = GUIDE_FIG(varargin)
 
 % Edit the above text to modify the response to help GUIDE_FIG
 
-% Last Modified by GUIDE v2.5 19-Apr-2018 14:44:11
+% Last Modified by GUIDE v2.5 30-Apr-2018 19:15:52
 
 % Begin initialization code - DO NOT EDIT
 gui_Singleton = 1;
@@ -247,4 +247,15 @@ cameratoolbar('NoReset')
 cameratoolbar('SetMode', 'orbit')
 
 
+% --- Executes on button press in pushbutton3.
+function pushbutton3_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton3 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
 
+
+% --- Executes on button press in pushbutton4.
+function pushbutton4_Callback(hObject, eventdata, handles)
+% hObject    handle to pushbutton4 (see GCBO)
+% eventdata  reserved - to be defined in a future version of MATLAB
+% handles    structure with handles and user data (see GUIDATA)
